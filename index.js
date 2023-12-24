0aaa120cda5e43a2f1cdb8e5dcb84cf6bf047d0e
mansi-ag25 <agrawalmansi547@gmail.com>
2023-12-24 22:17:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {message:"type your url here:",
+    name:"URL"}
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url=answers.URL;
+    var qr_svg = qr.image(url);
+qr_svg.pipe(fs.createWriteStream('qr_code.png'));
+ 
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
